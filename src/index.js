90407b88d0f6ed68f6534ea6decd4b413e7f9e2e
maxshelepen <maxshelepen25@gmail.com>
2020-02-23 16:19:26 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    return n.toString().split('').filter((el,indx,arr) => {
+        return el !== '-' ;
+        }).reverse().filter((el, indx, arra) => {
+          return el[indx] !== '0' ;
+        }).join('');
+    
 }
